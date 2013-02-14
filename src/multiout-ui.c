1877f3e2c9bdc9bfef45c26118019640fa296932
Patrick <boosthardware@gmail.com>
2013-02-14 17:36:23 +0200

    temporarily disable clutter deps

--- src/multiout-ui.c
+++ src/multiout-ui.c
@@ -17,8 +17,8 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <math.h>
-#include <clutter/clutter.h>
-#include <clutter-gtk/clutter-gtk.h>
+//#include <clutter/clutter.h>
+//#include <clutter-gtk/clutter-gtk.h>
 
 
 #include "process.h"
@@ -32,33 +32,33 @@
 
 //extern int global_gui; 
 
-ClutterActor *stage = NULL;
+//ClutterActor *stage = NULL;
 
 void multiout_ui_build (GtkWidget *window)
 {
 
   GtkWidget *hbox = GTK_BUTTON(lookup_widget(multiout_window, "hbox_w4_1"));
-  ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff }; /* Black */
+ // ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff }; /* Black */
 
-  gtk_clutter_init (NULL, NULL);
+ // gtk_clutter_init (NULL, NULL);
 
 
   /* Create the clutter widget: */
-  GtkWidget *clutter_widget = gtk_clutter_embed_new ();
-  gtk_box_pack_start (GTK_BOX (hbox), clutter_widget, TRUE, TRUE, 0);
-  gtk_widget_show (clutter_widget);
+ // GtkWidget *clutter_widget = gtk_clutter_embed_new ();
+ // gtk_box_pack_start (GTK_BOX (hbox), clutter_widget, TRUE, TRUE, 0);
+ // gtk_widget_show (clutter_widget);
 
   /* Set the size of the widget, 
    * because we should not set the size of its stage when using GtkClutterEmbed.
    */ 
-  gtk_widget_set_size_request (clutter_widget, 200, 200);
+  //gtk_widget_set_size_request (clutter_widget, 200, 200);
 
   /* Get the stage and set its size and color: */
-  stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (clutter_widget));
-  clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+ // stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (clutter_widget));
+ // clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
 
   /* Show the stage: */
-  clutter_actor_show (stage);	
+ // clutter_actor_show (stage);	
 
 }
 
