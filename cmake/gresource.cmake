7f0bd21fc5514ddffa998c12c9924ae6c44052e7
Sean Champ <spchamp@users.noreply.github.com>
2026-07-12 17:00:19 -0700

    Updating the implementation of add_resource_file (CMake)

--- cmake/gresource.cmake
+++ cmake/gresource.cmake
@@ -5,32 +5,34 @@ find_program(JSON_GLIB_FORMAT_BIN json-glib-format)
 
 find_program(GLIB_COMPILE_RESOURCES_BIN glib-compile-resources)
 
-include(GenerateExportHeader) # ?
-
-macro(add_resource_file _name _tgt)
-  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_name}.gresource.xml)
-    message(FATAL_ERROR "resource file not found: ${_name}.gresource.xml")
+function(add_resource_file _basename _tgt _dst)
+  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_basename}.gresource.xml)
+    message(FATAL_ERROR "resource file not found: ${_basename}.gresource.xml")
   endif()
 
-  ## per glib-compile-resources(1)
+  ## environment variables for glib-compile-resources(1)
   set(ENV{XMLLINT} ${XMLLINT_BIN})
   set(ENV{JSON_GLIB_FORMAT} ${JSON_GLIB_FORMAT_BIN})
-  set(ENV{CC} ${CMAKE_C_COMPILER})
+  # set(ENV{CC} ${CMAKE_C_COMPILER})
 
   add_custom_command(
-    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${_tgt}.c"
+    OUTPUT "${_dst}"
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     COMMAND ${GLIB_COMPILE_RESOURCES_BIN} ARGS
     --sourcedir="${CMAKE_CURRENT_SOURCE_DIR}"
-    --target="${CMAKE_CURRENT_BINARY_DIR}/${_tgt}.c"
+    --target="${_dst}"
     --generate-source
-    "${CMAKE_CURRENT_SOURCE_DIR}/${_name}.gresource.xml"
-    MAIN_DEPENDENCY "${CMAKE_CURRENT_SOURCE_DIR}/${_name}.gresource.xml"
-    ## Usage: list source files referred in the resource XML as args after 'tgt'
-    DEPENDS ${ARGN}
-    "${CMAKE_CURRENT_SOURCE_DIR}/${_name}.gresource.xml"
+    "${CMAKE_CURRENT_SOURCE_DIR}/${_basename}.gresource.xml"
+    MAIN_DEPENDENCY "${CMAKE_CURRENT_SOURCE_DIR}/${_basename}.gresource.xml"
+    ## Usage: list source files referred in the resource XML as args after 'tgt',
+    ## to ensure that the resource file should be rebuilt after any of those
+    ## files has been updated
+    DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${_basename}.gresource.xml" ${ARGN}
+    DEPENDS_EXPLICIT_ONLY
   )
 
-  add_library(${_tgt} STATIC ${CMAKE_CURRENT_BINARY_DIR}/${_tgt}.c)
+  # by side effect, ensure that the _dst file is generated before building 
+  # any tgts that would use _dst as a source file. 
+  add_custom_target(${_tgt} DEPENDS ${_dst})
 
-  target_link_libraries(${_tgt} PkgConfig::GTKMM)
-endmacro()
+endfunction()
